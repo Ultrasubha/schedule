26692c1e696eca038b34bd4360a3a1730eb96b76
Subhadeep Mandal <ultrasubha@gmail.com>
2022-03-26 14:19:51 +0530

    sads

--- script.js
+++ script.js
@@ -83,7 +83,7 @@
 				//"Date @ Time @ Task @ Duration @ <a href=\" \"> Name </a>",
 				"Mar 25 @ - @ GDSC Mini-Hackathon @ - @ - @Deadline",
 				"Mar 25 @ 3:00 pm @ CodeChef @ 3days @ <a href=\"https://www.codechef.com\">CodeChef</a>@competition",
-				"Mar 27 @ 10:00 am @ PEV CA @ 1hr @ <a href=\"https://myclass.lpu.in/\">MyClass</a>@LPU",
+				"Mar 28 @ 6:00 pm @ PEV CA @ 1hr @ <a href=\"https://myclass.lpu.in/\">MyClass</a>@LPU",
 				"Mar 28 @ - @ Network Assignment @ - @ <a href=\"https://myclass.lpu.in/\">MyClass</a>@LPU",
 				"Mar 29 @ - @ Sharda CTF @ 2days @ <a href=\"https://shardacyber.com/shardactf.html\">Sharda CTF</a>@competition",
 				"Mar 30 @ 8:00 pm @ CodeChef @ 3hrs @ <a href=\"https://www.codechef.com\">CodeChef</a>@competition",
