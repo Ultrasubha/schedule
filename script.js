c4db8f786cb20c2e2b190e4c85283ec02e6de603
[Subhadeep Mandal] <[ultrasubha@gmail.com]>
2022-04-19 19:58:58 +0530

    dfs

--- script.js
+++ script.js
@@ -82,6 +82,7 @@
 				//"Date @ Time @ Task @ Duration @ Venue",
 				//"Date @ Time @ Task @ Duration @ <a href=\" \"> Name </a>",
 				"Apr 20 @ 10:30 am @ GDSC Interview @ 30mins @ <a href=\"https://meet.google.com/\">Gmeet</a>@Deadline",
+				"Apr 20 @ 5:30 am @ Accenture's Tech Thrivers @ - @ - @Deadline",
 				"Apr 20 @ 8:00 pm @ CodeChef @ 3hrs @ <a href=\"https://www.codechef.com\">CodeChef</a>@competition",
 				"Apr 21 @ 1:00 pm @ Unity MCQ @ 1hr @ <a href=\"https://myclass.lpu.in/\">MyClass</a>@LPU",
 				"Apr 21 @ 8:00 pm @ CodeForces @ 2hrs @ <a href=\"https://codeforces.com/contests/1659\">CodeForces</a>@competition",
